5cba4b7bcbe3a4a18098764fcf32c3b4b8d1468f
Cata <ole.kh@hotmail.com>
2025-09-25 02:54:27 +0200

    Added: Build CI deployment to GH pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,10 @@ const config = {
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex()],
 	kit: { adapter: adapter() },
-	extensions: ['.svelte', '.svx']
+	extensions: ['.svelte', '.svx'],
+	paths: {
+		base: process.env.NODE_ENV === 'production' ? '/cata.moe' : ''
+	}
 };
 
 export default config;
